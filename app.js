7590b414f6ece4c419d693c132da62d085ae2055
Kalyan Chakravarthy Jogula <jogulakalyan8@gmail.com>
2024-04-11 19:38:56 -0500

    first commit

--- app.js
+++ app.js
@@ -3,7 +3,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy
 var bird = require("./models/bird");
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
+  })
+)
 
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
@@ -32,6 +54,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//lab13
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -41,13 +72,22 @@ app.use('/grid', gridRouter);
 app.use('/pick', pickRouter);
 app.use('/resource', resourceRouter);
 
+
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -58,42 +98,46 @@ app.use(function(err, req, res, next) {
 });
 
 
-async function recreateDB(){
-// Delete everything
-await bird.deleteMany();
-let instance1 = new
-bird({bird_name:"Blue Jay", color:'Blue and white',lifespan:8.0});
-instance1.save().then(doc=>{
-console.log("First object saved")}
-).catch(err=>{
-console.error(err)
-});
+async function recreateDB() {
+  // Delete everything
+  await bird.deleteMany();
+  let instance1 = new
+    bird({ bird_name: "Blue Jay", color: 'Blue and white', lifespan: 8.0 });
+  instance1.save().then(doc => {
+    console.log("First object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 
-let instance2 = new
-bird({bird_name:"Scarlet Macaw", color:'Brilliant red plumage', lifespan: 40.0});
-instance2.save().then(doc=>{
-console.log("Second object saved")}
-).catch(err=>{
-console.error(err)
-});
+  let instance2 = new
+    bird({ bird_name: "Scarlet Macaw", color: 'Brilliant red plumage', lifespan: 40.0 });
+  instance2.save().then(doc => {
+    console.log("Second object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 
-let instance3 = new
-bird({bird_name:"Bald Eagle", color:'brown', lifespan: 20.0});
-instance3.save().then(doc=>{
-console.log("Third object saved")}
-).catch(err=>{
-console.error(err)
-});
+  let instance3 = new
+    bird({ bird_name: "Bald Eagle", color: 'brown', lifespan: 20.0 });
+  instance3.save().then(doc => {
+    console.log("Third object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 
 }
 let reseed = false;
-if (reseed) {recreateDB();}
+if (reseed) { recreateDB(); }
 
 //Get the default connection
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
 
 module.exports = app;
