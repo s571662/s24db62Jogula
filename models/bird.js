651da694e6f1bd4086dba6ea87624c8c3082be40
Kalyan Chakravarthy Jogula <jogulakalyan8@gmail.com>
2024-04-15 20:53:17 -0500

    first commit

--- models/bird.js
+++ models/bird.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose")
 const birdSchema = mongoose.Schema({
-bird_name: String,
-color: String,
-lifespan: Number
+    bird_name: {
+        type: String,
+        minlength: 8,
+        maxlenght: 56,
+    },
+    color: String,
+    lifespan: {
+        type: Number,
+        min: 12,
+        max: 20,
+    }
+
 })
 module.exports = mongoose.model("bird", birdSchema)
--- routes/bird.js
+++ routes/bird.js
@@ -1,17 +1,33 @@
 var express = require('express');
-const bird_controlers= require('../controllers/bird');
+var passport = require('passport');
+const bird_controlers = require('../controllers/bird');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
 /* GET birds */
-router.get('/', bird_controlers.bird_view_all_Page );
+router.get('/', bird_controlers.bird_view_all_Page);
 router.get('/birds/:id', bird_controlers.bird_detail);
 /* GET detail bird page */
 router.get('/detail', bird_controlers.bird_view_one_Page);
 /* GET create bird page */
-router.get('/create', bird_controlers.bird_create_Page);
+router.get('/create',secured, bird_controlers.bird_create_Page);
 /* GET create update page */
-router.get('/update', bird_controlers.bird_update_Page);
+router.get('/update', secured, bird_controlers.bird_update_Page);
 /* GET delete bird page */
-router.get('/delete', bird_controlers.bird_delete_Page);
+router.get('/delete',secured, bird_controlers.bird_delete_Page);
+
+router.post('/login', passport.authenticate('local'), function (req, res) {
+    res.redirect('/');
+});
 
 
 module.exports = router;
